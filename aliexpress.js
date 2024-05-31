691efbb77d9697f2347051bc49e1d036a4cf4521
Santiago Arias <santi99oca@gmail.com>
2024-05-31 13:38:56 -0300

    Redirecciona la pagina de aliexpress a español y con la url correcta de item

---
+++ aliexpress.js
@@ -0,0 +1,22 @@
+let newLocation = location.href;
+let refresh = false;
+if(location.href.includes('/item/')){
+    console.log('Aliexpress item page detected');
+    newLocation = newLocation.replace('/item/', '/i/');
+    refresh = true;
+}
+
+let regex = /https:\/\/(.+)\.aliexpress/
+let match = regex.exec(location.href);
+if(match){
+    console.log(match);
+    if(match[1] !== 'es' && match[1] !== 'www' && match[1] !== 'pt'){
+        console.log('Changing language to Spanish');
+        newLocation = newLocation.replace(match[1], 'es');
+        refresh = true;
+    }
+}
+
+if(refresh){
+    location.href = newLocation;
+}
